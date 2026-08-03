bbfe603c7a45919f89563b641d3d7bfccdbc7d42
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-03 21:31:24 +0530

    Add Day 03 Problem 01

---
+++ Day 03 (Recursion - Quiz)/problem01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int fun(int n)
+{
+    static int i=1;
+    if(n>=5) return n;
+
+    n = n + i;
+    i++;
+    return fun(n);
+}
+int main()
+{
+    int r = fun(1);
+    cout<<r;
+    return 0;
+}
