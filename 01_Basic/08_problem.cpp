e3e0233ff01f5009d586713f977136ba955fd189
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 21:54:22 +0530

    Add problem 08

---
+++ 01_Basic/08_problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,sum;
+    cout<<"Enter the Number x :";
+    cin>>x;
+    cout<<"Enter the Number Y :";
+    cin>>y;
+    sum = x + y;
+    cout<<"Sum of x and y is :"<<sum;
+    return 0;
+}
+//only $ anc _ are allowed in variable.
