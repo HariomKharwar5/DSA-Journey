c0457184c4b9fe749c1cae50607a21e872ec50b6
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:45:56 +0530

    Add Loop Problem 04

---
+++ 05_Loop_Statement_02/04_problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // do while loop  execute once even if condition does not satisfy...
+    int i = 11;
+    do
+    {
+        cout<<i<<" ";
+        i++;
+    } while (i<=10);
+    
+    return 0;
+}
