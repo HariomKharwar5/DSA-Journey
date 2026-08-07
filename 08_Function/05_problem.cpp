f3f4e6cd3dc5704550374b45f739e1762a27c909
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 23:16:02 +0530

    Add Function Problem 05

---
+++ 08_Function/05_problem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+//Library functions....
+{
+    cout<<sqrt(4)<<endl;//for Square Root : cbrt
+    cout<<cbrt(8)<<endl;//for cube Root : cbrt
+    cout<<min(3,6)<<endl;
+    cout<<max(3,6)<<endl;
+    cout<<pow(2,4)<<endl;
+}
