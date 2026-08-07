b05f181937008eb2484440fa0a0b0e47f4b233d2
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 21:54:21 +0530

    Add problem 04

---
+++ 01_Basic/04_problem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 7, y = 5;
+    cout<< x - y << endl;
+    cout<< x + y << endl;
+    cout<< x * y << endl;
+    cout<< x / y << endl;//Quotient.
+    cout<< x % y << endl;//Remainder.
+    return 0;
+}
