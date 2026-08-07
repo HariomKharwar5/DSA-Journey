f43f1ceec5418b1621d2afc4b2241670f88ee7c9
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:38:05 +0530

    Add Loop Problem 06

---
+++ 04_Loop_Statement/06_problem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    // for(int i =1;i<=2*n-1;i+=2)
+    // cout<<i<<" ";
+    int a = 1;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<a<<" ";
+        a+=2;
+    }
+        
+
+
+    return 0;
+}
