6c35d9dd3e3a7e2da3a31c8a0a81e2144a69bc11
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:23:51 +0530

    Add Conditional Problem 05

---
+++ 03_Conditional_Statement_02/05_problem.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter side a :";
+    cin>>a;
+    cout<<"Enter side b :";
+    cin>>b;
+    cout<<"Enter side c :";
+    cin>>c;
+    if(a>b)
+    {
+        if(a>c){
+            cout<<"a is largest. ";
+        }
+        else
+        {
+            cout<<"c is largest. ";
+        }
+    }
+    else
+    {
+        if(b>c)
+        {
+            cout<<" b is largest. ";
+        }
+        else
+        {
+            cout<<"c is largest. ";
+        }
+    }
+    
+    
+    return 0;
+}
