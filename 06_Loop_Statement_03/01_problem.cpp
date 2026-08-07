7b859f53f9119a60af05371f8675d993138550db
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:53:46 +0530

    Add Loop Problem 01

---
+++ 06_Loop_Statement_03/01_problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //practice problem
+    int i;
+    while(i=10)
+    {
+        cout<<i<<endl;
+        i+=1;
+    }
+    //It will run upto infinite loop......
+    return 0;
+}
