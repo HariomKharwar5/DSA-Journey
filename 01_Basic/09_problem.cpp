3ff7e52601f053a2bc687d809a5be03c3988ea96
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 21:54:23 +0530

    Add problem 09

---
+++ 01_Basic/09_problem.cpp
@@ -0,0 +1,15 @@
+// Take input and print half of it..
+// Type Casting...
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter the Number :";
+    cin>>x;
+    float y = float(x);
+    
+    cout<<y/2;
+    
+    return 0;
+}
