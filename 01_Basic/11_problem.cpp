bf47e73032483b9f0c39ddf2cf0dfb8f93b2638e
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 21:54:23 +0530

    Add problem 11

---
+++ 01_Basic/11_problem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // From digit to charACTER
+    int x;
+    cout<<"Enter the digit :";
+    cin>>x;
+    char ch = char(x);
+    cout<<ch;
+    return 0;
+}
