49022f14a46df89a8729b6ba887cb971fc5a0e8b
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:45:56 +0530

    Add Loop Problem 02

---
+++ 05_Loop_Statement_02/02_problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //counting from 1 to 100 to get all odd number.
+    for(int i=1;i<=100;i++)
+    {
+        if(i%2==0) continue;
+        {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
