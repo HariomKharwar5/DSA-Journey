619410f32c782290106c42feaa0ef41751a10dc2
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 23:21:52 +0530

    Add Pointer Problem 01

---
+++ 09_Pointer/01_problem.cpp
@@ -0,0 +1,15 @@
+//Pointers
+//the purpose of pointer is to access or update  the value of the vriable
+// whose addres is in the pointer using *operator/dereference.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 3;
+    cout<<&x<<endl;//Calculating address
+    
+    int y = 3;
+    cout<<&y<<endl;
+
+    return 0;
+}
