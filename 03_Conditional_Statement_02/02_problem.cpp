23f94c009b6ce3d54d23faff4a3ad897ac37f9d9
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:23:51 +0530

    Add Conditional Problem 02

---
+++ 03_Conditional_Statement_02/02_problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a Number :";
+    cin>>n;
+    if(n%3 == 0 && n%5 == 0)
+    {
+        cout<<"Enter Number is divisible by both 3 and 5 ";
+    }
+    else
+    {
+        cout<<"Enter Number is not divisible by both 3 and 5 ";
+    }
+    return 0;
+}
