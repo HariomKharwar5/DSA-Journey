e5fdb6d683de9766f757f50532935cff07feb586
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:03:47 +0530

    Add Conditional Problem 01

---
+++ 02_Conditional_Statement/01_problem.cpp
@@ -0,0 +1,15 @@
+//Conditional statement...
+// CHeck Even Odd Condition...
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter a Number :";
+    cin>>x;
+    if(x%2==0)
+        cout<<"Even Number";
+    else 
+        cout<<"Odd Number";
+    return 0;
+}
