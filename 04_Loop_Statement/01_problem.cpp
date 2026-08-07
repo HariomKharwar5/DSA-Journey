39e026a405d2dc5bcc19c4655e22fe8d76f1c607
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 22:36:48 +0530

    Add Loop Problem 01

---
+++ 04_Loop_Statement/01_problem.cpp
@@ -0,0 +1,13 @@
+//Loop:-----Repeatation
+//For Loop....
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the Number :";
+    cin>>n;
+    for(int i = 1; i<=n; i++)
+        cout<<"Hello World"<<endl;
+    return 0;
+}
