0b2f61f4522c9af33789369742791f9d53fe7e94
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 23:09:11 +0530

    Add Pattern Printing Problem 05

---
+++ 07_Pattern_Printing/05_problem.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+        
+    cout<<"Enter Rows :";
+    cin>>m;
+    cout<<"Enter column :";
+    cin>>n;
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<char(j+64)<<" ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
