88c70c4cc0d5cb5855573c8b7023fe1ba4c27b8d
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 21:54:21 +0530

    Add problem 05

---
+++ 01_Basic/05_problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Pre and post increment and decrement.....
+    int x = 5;
+    x++;
+    cout<<x<<endl;
+    x--;
+    cout<<x<<endl;
+    ++x;
+    cout<<x<<endl;
+    --x;
+    cout<<x<<endl;
+    return 0;
+}
