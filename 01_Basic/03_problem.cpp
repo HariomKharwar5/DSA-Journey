c94cbf410d34f14ddbb4e965d8a717e94f51ff40
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-07 21:54:21 +0530

    Add problem 03

---
+++ 01_Basic/03_problem.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int x = 5;
+    cout<<x<<endl;
+    // int x = 7;//it's error because we redeclared the value of int
+    // cout<<x<<endl;
+    // x = x + 5;
+    // cout<<x<<endl;
+    // x = x - 10;
+    // cout<<x<<endl;
+    return 0;
+}
