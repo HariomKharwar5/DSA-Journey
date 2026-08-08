4f3b8bc5b9994f1aa71fde45e2cae2357733839b
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-08 22:04:06 +0530

    Added Recursion basics

---
+++ 10_Recursion-BASIC/07_problem.cpp
@@ -0,0 +1,16 @@
+//Calculate FActorial using Recursion.
+#include<iostream>
+using namespace std;
+int fact(int n)
+{
+    if(n==0 || n==1) return 1;
+    return n * fact(n-1);
+}
+int main()
+{
+    int n;
+    cout<<"Enter N :";
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
