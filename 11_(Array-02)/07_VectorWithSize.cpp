00ba4d0c428da6fd059fb1f0d920217f57f0f062
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-10 23:55:03 +0530

    Add vector with size example

---
+++ 11_(Array-02)/07_VectorWithSize.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v(5);
+    // vector<int> v(5,7);//initial size = 5, and each element is 7.
+    cout<<v.size()<<" ";
+    cout<<v.size()<<endl;
+    cout<<v[0];
+    
+}
