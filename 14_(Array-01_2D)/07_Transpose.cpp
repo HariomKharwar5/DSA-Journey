bdee3409e5ca35d36db54133ff3469f6f46a5b72
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-19 00:09:53 +0530

    Add 07_Transpose.cpp

---
+++ 14_(Array-01_2D)/07_Transpose.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cout<<"Enter row : ";
+    cin>>n;
+
+    cout<<"Enter column : ";
+    cin>>m;
+
+    int arr[n][m];
+  
+    cout<<"Enter the Elements : ";
+    for(int i = 0; i<n ; i++)
+    {
+        for(int j = 0; j<m ; j++)
+        {
+            cin>>arr[i][j];
+        }
+        
+    }
+    for(int i = 0; i<n ; i++)
+    {
+        for(int j = 0; j<m ; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        
+        cout<<endl;
+    }
+
+    cout<<endl;
+    //Printing Transpose....
+    for(int j = 0; j<m ; j++)
+    {
+        for(int i = 0; i<n ; i++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
