699882479f5297dc2ab2bacd5e5199128448d324
Hariom Kharwar <hariomkumarkharwar@gmail.com>
2026-08-20 13:41:42 +0530

    Add 03_WavePrint02.cpp

---
+++ 15_(Array-02_2D)/03_WavePrint02.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+
+    cout << "Enter row: ";
+    cin >> m;
+
+    cout << "Enter column: ";
+    cin >> n;
+
+    int arr[m][n];
+
+    cout << "Enter the Elements: ";
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Wave Elements are: " << endl;
+
+    for(int i = m-1; i >= 0; i--)
+    {
+        if(i % 2 == 0)
+        {
+            // Left to Right
+            for(int j = 0; j < n; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            // Right to Left
+            for(int j = n - 1; j >= 0; j--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
